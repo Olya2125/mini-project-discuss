87e7d11c6429fe75a35b11009d3d6b31de88e894
Olya2125 <139589012+Olya2125@users.noreply.github.com>
2024-05-06 13:42:06 +0300

    visual header

--- src/components/header/page.tsx
+++ src/components/header/page.tsx
@@ -1,55 +1,74 @@
-
-
-import React from 'react';
-import Link from 'next/link';
-import { Button } from "@nextui-org/react";
+import React from "react";
 import * as action from "@/actions";
 import { auth } from "@/auth";
-import "./header.css";
-import { useState } from 'react';
+import Logo from "@/components/logo/page";
+import { Link, Button, Input } from "@nextui-org/react";
 
-
-export default async function  ()  {
+export default async function () {
   const session = await auth();
 
   return (
-    <main>
-    <header className="header">
-      <nav>
-        <ul>
-          <li>
-            <Link href="/">
-              <h2>Discuss</h2>
-            </Link>
-          </li>
-          <li>
-            <Link href="/about">
-              <h2>Search</h2>
-            </Link>
-          </li>
-          <li >
-          <form action={action.signIn}>
-        <Button type="submit">Sign in</Button>
-      </form>
-      <form action={action.signOut}>
-        <Button type="submit">Sign out</Button>
-      </form>
+    <main className="bg-blue-900">
+      <header className="header">
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">
+                <Logo />
+              </Link>
+            </li>
+            <li>
+              <Link href="">
+                <Input
+                  classNames={{
+                    base: "max-w-full sm:max-w-[10rem] h-10",
+                    mainWrapper: "h-full",
+                    input: "text-small",
+                    inputWrapper:
+                      "h-full font-normal text-default-500 bg-default-500/40 dark:bg-default-800/20",
+                  }}
+                  placeholder="Type to search..."
+                  size="sm"
+                  type="search"
+                />
+              </Link>
+            </li>
+            <li className="hidden lg:flex">
+              <form action={action.signIn}>
+                <Button
+                  className="bg-blue-200"
+                  variant="solid"
+                  size="md"
+                  radius="sm"
+                  type="submit"
+                >
+                  Sign in
+                </Button>
+              </form>
+              <form action={action.signOut}>
+                <Button
+                  className="btn-signout"
+                  color="primary"
+                  size="md"
+                  radius="sm"
+                  variant="ghost"
+                  type="submit"
+                >
+                  Sign out
+                </Button>
+              </form>
 
-      {session?.user ? (
-        <div>
-          <h3>Signed in</h3>
-          <p>{JSON.stringify(session.user)}</p>
-        </div>
-      ) : (
-        <div>signed out</div>
-      )}
-          </li>
-        </ul>
-      </nav>
-    </header>
+              {session?.user ? (
+                <div>
+                  <h3>Signed in</h3> <p>{JSON.stringify(session.user)}</p>
+                </div>
+              ) : (
+                <div>signed out</div>
+              )}
+            </li>
+          </ul>
+        </nav>
+      </header>
     </main>
   );
-};
-
-
-
+}
