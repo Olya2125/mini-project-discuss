3cb64de90ec065cdf558e2757c59db237dcaff2c
TatyVerasen <tat.ksl@outlook.com>
2024-05-21 20:16:23 +0300

    design comments page

--- src/app/viewPost/page.tsx
+++ src/app/viewPost/page.tsx
@@ -2,6 +2,8 @@ import Header from "@/components/header/page";
 import React from "react";
 import {Textarea} from "@nextui-org/input";
 import { Button } from "@nextui-org/react";
+import { Avatar } from "@nextui-org/react";
+import styles from "@/components/styles.module.css"
 
 
 
@@ -9,17 +11,39 @@ export default function ViewPost() {
   return (
     <div>
       <Header />
-<div>
-    <h3>Implementing Charts</h3>
-    <p>I'm trying to add a chart into my application, can anyone help me out?</p>
-</div>
+<div className="flex flex-col items-center p-10">
+
+    <h3 className={styles.alltitle}>Implementing Charts</h3>
+    <p className={styles.application}>I'm trying to add a chart into my application, can anyone help me out?</p>
+
 <Textarea
       placeholder="Enter your description"
-      className="max-w-xs"
+      className={styles.textarea}
     />
 
-    <Button>Save</Button>
-    <p>All 20 comments</p>
+    <Button 
+    color="primary"
+    variant="solid"
+    size="md"
+    radius="sm"
+    type="submit"
+    >Save</Button>
+    <p className={styles.application}>All 20 comments</p>
+    
+    </div>
     </div>
   );
 }
+
+
+// className="application"
+
+// import Block from 'react-blocks';
+// npm install react-blocks
+//function MyComponent() {
+//  return (
+//    <Block layout="horizontal">
+//      <Block>Block 1</Block>
+//      <Block>Block 2</Block>
+//    </Block>
+//  );
